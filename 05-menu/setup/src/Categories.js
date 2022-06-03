a7bdb039b7f02f97cb77e22c83ff8fd74e356336
Radek <r.ciepichal@gmail.com>
2022-06-03 21:41:49 +0200

    05

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {/* <button className="filter-btn" onClick={() => filterItems('all')}>
+        All
+      </button>
+      <button className="filter-btn" onClick={() => filterItems('breakfast')}>
+        Breakfast
+      </button> */}
+      {categories.map((category, idx) => {
+        return (
+          <button className="filter-btn" key={idx} onClick={() => filterItems(category)}>
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Categories;
--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = (props) => {
+  // const items = [props];
+  // console.log(items);
+  return (
+    // <div></div>
+    <div className="section-center">
+      {props.items.map((menuItem) => {
+        // console.log(menuItem);
+        const { id, title, img, price, desc } = menuItem;
+        return (
+          <article key={id} className="menu-item">
+            <img src={img} alt={title} className="photo" />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">${price}</h4>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Menu;
